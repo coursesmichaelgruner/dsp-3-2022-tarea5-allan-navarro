c187ec0e29f3f1b2260f53bf5033500acd3df70b
Allan Navarro <anavarro3106@gmail.com>
2022-11-15 18:30:59 -0600

    Add dft

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Sistema de Supresión de Ruido de Red Eléctrica
 
 > Presente su reporte aquí
+
+1. Fs = 48000 Hz
+2. ver imagen prob2.jpeg
--- tarea5.py
+++ tarea5.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
 from scipy.io import wavfile
+import numpy as np
+import matplotlib.pyplot as plt
 
 if len(sys.argv) != 2:
     print('please specify name of wav file')
@@ -9,3 +11,15 @@ if len(sys.argv) != 2:
 # Read wav file
 fs, data = wavfile.read(sys.argv[1])
 print(f'1. sample rate {fs}')
+
+fft = np.fft.fft(data)
+
+freq = np.fft.fftfreq(data.shape[-1])*fs
+
+plt.figure(1)
+plt.plot(freq, np.abs(fft))
+plt.xlabel("Freqency (Hz)")
+plt.ylabel("Magnitude")
+plt.title("DFT")
+
+plt.show()
