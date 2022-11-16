bba02f7eb6e976f35eb40769244c8e115530e0a5
Allan Navarro <anavarro3106@gmail.com>
2022-11-15 19:05:36 -0600

    3

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 > Presente su reporte aquí
 
 1. Fs = 48000 Hz
-2. ver imagen prob2.jpeg
+2. La imagen prob2_a.png muestra el espectro completo, por otro lado al aumentar una region del mismo, en la figura prob2_b.png. Se observa que el espectro tiene simetría lo cual es esperado para una señal real.
--- tarea5.py
+++ tarea5.py
@@ -16,10 +16,26 @@ fft = np.fft.fft(data)
 
 freq = np.fft.fftfreq(data.shape[-1])*fs
 
-plt.figure(1)
-plt.plot(freq, np.abs(fft))
-plt.xlabel("Freqency (Hz)")
-plt.ylabel("Magnitude")
-plt.title("DFT")
+plt.figure('prob2')
+plt.plot(freq, fft.real,freq, fft.imag)
+plt.legend(['Real','Imag'])
+plt.xlabel("Frecuencia (Hz)")
+plt.ylabel("DFT")
+
+
+freq_split = freq[:len(freq)//2]
+fft_split = fft[:len(fft)//2]
+
+freq_split= freq_split[:len(fft)//24]
+fft_split= fft_split[:len(fft)//24]
+
+harmonics = np.arange(1,30)*60
+
+plt.figure('prob3')
+plt.plot(freq_split, np.abs(fft_split))
+plt.vlines(harmonics,ymin=0,ymax=np.max(np.abs(fft_split)),linestyles='dotted',colors='red')
+plt.legend(['DFT','armónicos'])
+plt.xlabel("Frecuencia (Hz)")
+plt.ylabel("Magnitud (DFT)")
 
 plt.show()
