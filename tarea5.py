f3f7ba058e0130b15f410c248b2320b24c787907
Allan Navarro <anavarro3106@gmail.com>
2022-11-15 21:53:30 -0600

    filter

--- tarea5.py
+++ tarea5.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import sys
 from scipy.io import wavfile
+import scipy.signal
+from scipy.signal import lfilter
 import numpy as np
 import matplotlib.pyplot as plt
+from math import cos, pi
 
 if len(sys.argv) != 2:
     print('please specify name of wav file')
@@ -39,14 +42,51 @@ plt.legend(['DFT', 'armónicos'])
 plt.xlabel("Frecuencia (Hz)")
 plt.ylabel("Magnitud (DFT)")
 
-fig, ax = plt.subplots(nrows=2, ncols=2)
+# fig, ax = plt.subplots(nrows=2, ncols=2)
 
-for i in range(4):
-    ax[i//2, i % 2].vlines(harmonics, ymin=0, ymax=10,
-                           linestyles='dotted', colors='red')
+# for i in range(1):
+#     f,t,Zxx = scipy.signal.stft(data,fs,nperseg=256)
 
-fig.canvas.manager.set_window_title('prob4')
+#     # fft_split = stft[:len(stft)//2]
 
-fig.tight_layout()
+#     # freq_split = freq_split[:len(fft)//24]
+#     # fft_split = fft_split[:len(fft)//24]
+
+#     ax[i//2, i % 2].pcolormesh(t,f,np.abs(Zxx),vmin=0,vmax=np.max(data), shading='gouraud')
+#     ax[i//2, i % 2].set_ylim([f[1], f[-1]])
+#     ax[i//2, i % 2].set_yscale('log')
+
+#     # ax[i//2, i % 2].vlines(harmonics, ymin=0, ymax=10,
+#     #                        linestyles='dotted', colors='red')
+
+# fig.canvas.manager.set_window_title('prob4')
+
+# fig.tight_layout()
+
+def get_qs(f0):
+    w0 = 2*pi*f0/fs
+    r=0.999
+    b=[1,-2*cos(w0),1]
+    a=[1,-2*r*cos(w0),r*r]
+    return b,a
+
+
+res=data
+for h in np.arange(1,10)*60:
+    coeffs=get_qs(h)
+    res=lfilter(coeffs[0],coeffs[1],res)
+
+res = res.astype(np.int16)
+
+wavfile.write("result.wav",fs,res)
+
+fft = np.fft.fft(res)
+
+freq = np.fft.fftfreq(res.shape[-1])*fs
+
+plt.figure('filt')
+plt.plot(freq, np.abs(fft))
+plt.xlabel("Frecuencia (Hz)")
+plt.ylabel("Mag. DFT")
 
 plt.show()
